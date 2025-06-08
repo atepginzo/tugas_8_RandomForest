657e9afdeb5cecea8e4ddd18171cc8155c90dce1
atepginzo <70021476+atepginzo@users.noreply.github.com>
2025-06-09 03:31:09 +0700

    guncorn

--- aplikasi/app.py
+++ aplikasi/app.py
@@ -94,4 +94,4 @@ def show_img(img):
 if __name__ == '__main__':
     import os
     port = int(os.environ.get('PORT', 8080))
-    app.run(debug=False, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
