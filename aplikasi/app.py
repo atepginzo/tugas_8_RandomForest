9b8cf120d826d70a122217c4b5b582cbf23e8f1c
atepginzo <70021476+atepginzo@users.noreply.github.com>
2025-06-09 03:36:19 +0700

    update lagi

--- aplikasi/app.py
+++ aplikasi/app.py
@@ -92,6 +92,5 @@ def show_img(img):
     return send_from_directory('../visualizations', img)
 
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get('PORT', 8080))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run()
+
